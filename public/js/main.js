43f9a1ff6a9b81d7f25dc2d310a90bf744207194
Nicolas Wielonsky <nicolas.wielonsky@gmail.com>
2017-07-19 08:43:39 +0200

    Add Dropzone to multiple upload image

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  // $("#dropzone").dropzone({ url: "/file/post", uploadMultiple: true });
+  Dropzone.clickable = false;
+  
 });
+
+// Dropzone.options = {
+//   uploadMultiple: true,
+//   acceptedFiles: 'image/*',
+//   maxFilesize: 2,
+//   clickable: false,
+//   autoProcessQueue: false,
+//   dictDefaultMessage: 'test'
+// };
